5e1449ba809ba84496315d8be72eed9812a1ae89
Florent Carrez <florent.carrez.fc@gmail.com>
2024-02-02 22:43:06 +0100

    feat(operator): first try for operator function

---
+++ operator/functions.go
@@ -0,0 +1,111 @@
+package operator
+
+import "reflect"
+
+type integerType interface {
+	~int | ~int8 | ~int16 | ~int32 | ~int64 |
+		~uint | ~uint8 | ~uint16 | ~uint32 | ~uint64
+}
+
+type floatType interface {
+	~float32 | ~float64
+}
+
+type complexType interface {
+	~complex64 | ~complex128
+}
+
+type numberTypes interface {
+	integerType | floatType | complexType
+}
+
+type textTypes interface {
+	~string | ~rune
+}
+
+func BuiltinPlus[T interface{ numberTypes | textTypes }](left, right T) T {
+	return left + right
+}
+
+func BuiltinMinus[T interface{ numberTypes }](left, right T) T {
+	return left - right
+}
+
+func BuiltinMult[T interface{ numberTypes }](left, right T) T {
+	return left * right
+}
+
+func BuiltinDiv[T interface{ numberTypes }](left, right T) T {
+	return left / right
+}
+
+func BuiltinMod[T interface{ integerType }](left, right T) T {
+	return left % right
+}
+
+func operatorWrapper(f any) func(any, any) any {
+	return func(left, right any) any {
+		return reflect.ValueOf(f).Call([]reflect.Value{reflect.ValueOf(left), reflect.ValueOf(right)})[0].Interface()
+	}
+}
+
+var plusOperator map[reflect.Kind]func(any, any) any
+
+func innerStructPlus(left, right any) any {
+	leftValue := reflect.ValueOf(left)
+	rightValue := reflect.ValueOf(right)
+	returnValue := reflect.New(leftValue.Type())
+
+	for i := 0; i < leftValue.NumField(); i++ {
+		leftField := leftValue.Field(i)
+		rightField := rightValue.Field(i)
+		returnField := returnValue.Elem().Field(i).Addr()
+		plus, exists := plusOperator[leftField.Kind()]
+		if !exists {
+			continue
+		}
+		returnField.Elem().Set(reflect.ValueOf(plus(leftField.Interface(), rightField.Interface())))
+	}
+	return returnValue.Elem().Interface()
+}
+
+func init() {
+	plusOperator = map[reflect.Kind]func(any, any) any{
+		reflect.Float32:    operatorWrapper(BuiltinPlus[float32]),
+		reflect.Float64:    operatorWrapper(BuiltinPlus[float64]),
+		reflect.Int:        operatorWrapper(BuiltinPlus[int]),
+		reflect.Int16:      operatorWrapper(BuiltinPlus[int16]),
+		reflect.Int32:      operatorWrapper(BuiltinPlus[int32]),
+		reflect.Int64:      operatorWrapper(BuiltinPlus[int64]),
+		reflect.Int8:       operatorWrapper(BuiltinPlus[int8]),
+		reflect.Uint:       operatorWrapper(BuiltinPlus[uint]),
+		reflect.Uint8:      operatorWrapper(BuiltinPlus[uint8]),
+		reflect.Uint16:     operatorWrapper(BuiltinPlus[uint16]),
+		reflect.Uint32:     operatorWrapper(BuiltinPlus[uint32]),
+		reflect.Uint64:     operatorWrapper(BuiltinPlus[uint64]),
+		reflect.String:     operatorWrapper(BuiltinPlus[string]),
+		reflect.Complex64:  operatorWrapper(BuiltinPlus[complex64]),
+		reflect.Complex128: operatorWrapper(BuiltinPlus[complex128]),
+		reflect.Struct:     innerStructPlus,
+	}
+}
+
+func StructPlus[T any](left, right T) T {
+	if reflect.TypeOf(left).Kind() != reflect.Struct {
+		panic("StructPlus must be called with structure type")
+	}
+	return innerStructPlus(left, right).(T)
+}
+
+// func StructMinus[T any](left, right T) T {
+
+// }
+// func StructMult[T any](left, right T) T {
+
+// }
+// func StructDiv[T any](left, right T) T {
+
+// }
+// func StructMod[T any](left, right T) T {
+
+// }
